a6222e8f5dd99cc5b3569de8ff72cdec9a703550
IanCode09 <ianlombu@protonmail.com>
2020-10-25 15:52:43 +0700

    User Auth Endpoint ; JSON Web Token ; Custom Auth Middleware

---
+++ backend/controllers/userController.js
@@ -0,0 +1,48 @@
+import User from '../models/userModel.js'
+import asyncHandler from 'express-async-handler'
+import generateToken from '../utils/generateToken.js'
+
+// @desc      Auth User & get Token
+// @router    POST /api/users/login
+// @access    Public
+const authUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body
+    
+    const user = await User.findOne({ email })
+
+    if( user && (await user.matchPassword(password))) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token:generateToken(user._id)
+        })
+    } else {
+        res.status(401)
+        throw new Error('Invalid User or Password')
+    }
+})
+
+
+// @desc      Get User Profile
+// @router    GET api/users/profile
+// @access    Private
+const getUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id)
+    
+    if(user) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        })
+    } else {
+        res.status(404)
+        throw new Error('User Not Found')
+    }
+})
+
+
+export { authUser, getUserProfile }
