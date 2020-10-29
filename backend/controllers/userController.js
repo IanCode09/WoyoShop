fc80e3aa3d9d58323e7747255bb210d730d2603f
IanCode09 <ianlombu@protonmail.com>
2020-10-29 23:07:13 +0700

    Get User By Id & Update User Endpoints

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -131,4 +131,54 @@ const deleteUser = asyncHandler(async (req, res) => {
     }
 })
 
-export { authUser, getUserProfile, registerUser, updateUserProfile, getUsers, deleteUser }
+
+// @desc      Get User by Id
+// @router    GET api/users/:id
+// @access    Private/Admin
+const getUserById = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id).select('-password')
+
+    if(user) {
+        res.json(user)
+    } else {
+        res.status(404)
+        throw new Error('User Not Found')
+    }
+})
+
+
+// @desc      Update User
+// @router    PUT api/users/:id
+// @access    Private
+const updateUser = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id)
+
+    if(user) {
+        user.name = req.body.name || user.name
+        user.email = req.body.email || user.email
+        user.isAdmin = req.body.isAdmin || user.isAdmin
+
+        const updateUser = await user.save()
+
+        res.json({
+            _id: updateUser._id,
+            name: updateUser.name,
+            email: updateUser.email,
+            isAdmin: updateUser.isAdmin,
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid User Data')
+    }
+})
+
+export { 
+    authUser, 
+    getUserProfile, 
+    registerUser, 
+    updateUserProfile, 
+    getUsers, 
+    deleteUser,
+    getUserById,
+    updateUser 
+}
