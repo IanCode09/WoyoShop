6d0321ee530b40b3740a0599bf04e1a83fd06667
IanCode09 <ianlombu@protonmail.com>
2020-10-30 23:55:23 +0700

    Admin Order List

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -84,4 +84,13 @@ const getMyOrders = asyncHandler(async (req, res) => {
     res.json(orders)
 })
 
-export { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders }
+
+// @desc      Get All Orders
+// @router    GET /api/orders
+// @access    Private/Admin
+const getOrders = asyncHandler(async (req, res) => {
+    const orders = await Order.find({}).populate('user', 'id name')
+    res.json(orders)
+})
+
+export { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders, getOrders }
