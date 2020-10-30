18b03b508f76a60d45531ba787cc72c80b550c12
IanCode09 <ianlombu@protonmail.com>
2020-10-30 11:54:57 +0700

    Create & Update Product Endpoints

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,5 +1,6 @@
 import Product from '../models/productModel.js'
 import asyncHandler from 'express-async-handler'
+import { updateUser } from './userController.js'
 
 // @desc      Fetch All Product
 // @router    GET /api/products
@@ -39,4 +40,65 @@ const deleteProduct =  asyncHandler(async (req, res) => {
     }
 })
 
-export { getProducts, getProductById, deleteProduct }
+
+// @desc      Create Product
+// @router    POST /api/products
+// @access    Private/Admin
+const createProduct =  asyncHandler(async (req, res) => {
+    const product = new Product({
+        name: 'Sample Name',
+        price: 0,
+        user: req.user._id,
+        image: '/images/sample.jpg',
+        brand: 'Sample Brand',
+        category: 'Sample Category',
+        countInStock: 0,
+        numReview: 0,
+        description: 'Sample Description'
+    })
+
+    const createProduct = await product.save()
+    res.status(201).json(createProduct)
+})
+
+
+// @desc      Update Product
+// @router    PUT /api/products/:id
+// @access    Private/Admin
+const updateProduct =  asyncHandler(async (req, res) => {
+    const {
+        name,
+        price, 
+        description,
+        image,
+        brand,
+        category,
+        countInStock
+    } = req.body
+
+    const product = await Product.findById(req.params.id)
+
+    if(product) {
+        product.name= name,
+        product.price = price, 
+        product.description= description,
+        product.image= image,
+        product.brand= brand,
+        product.category= category,
+        product.countInStock= countInStock
+
+        const updateProduct = await product.save()
+        res.json(updateProduct)
+    } else {
+        res.status(404)
+        throw new Error('Product Not Found')
+    }
+})
+
+export { 
+    getProducts, 
+    getProductById, 
+    deleteProduct, 
+    createProduct,
+    updateProduct
+}
