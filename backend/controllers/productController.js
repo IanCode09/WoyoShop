551c3038460077786ebcc7bb0a0a52020ebbe38d
IanCode09 <ianlombu@protonmail.com>
2020-10-31 12:49:49 +0700

    Morgan & Create Review Endpoint

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -95,10 +95,54 @@ const updateProduct =  asyncHandler(async (req, res) => {
     }
 })
 
+
+// @desc      Create New Review
+// @router    POST /api/products/:id/reviews
+// @access    Private
+const createProductReview =  asyncHandler(async (req, res) => {
+    const { rating, comment } = req.body
+
+    const product = await Product.findById(req.params.id)
+
+    if(product) {
+        const alreadyReviewd = product.reviews.find(
+            (r) => r.user.toString() === req.user._id.toString()
+        )
+
+        if(alreadyReviewd) {
+            res.status(400)
+            throw new Error('Product Already Reviewed')
+        }
+
+        const review = {
+            name: req.user.name,
+            rating: Number(rating),
+            comment,
+            user: req.user._id
+        }
+
+        product.reviews.push(review)
+
+        product.numReview = product.reviews.length
+
+        product.rating = product.reviews.reduce((acc, item) => item.rating + acc, 0) / product.reviews.length
+
+        await product.save()
+        res.status(201).json({ message: 'Review Added'})
+    } else {
+        res.status(404)
+        throw new Error('Product Not Found')
+    }
+})
+
+
+
+
 export { 
     getProducts, 
     getProductById, 
     deleteProduct, 
     createProduct,
-    updateProduct
+    updateProduct,
+    createProductReview
 }
